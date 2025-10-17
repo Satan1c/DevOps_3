8343e59d48896a6f30cee9581d2fdc9301579d57
Satan1c <satan1c@ukr.net>
2025-10-17 08:43:01 +0300

    sys_tool.py — сценарій тільки для прямого запуску

---
+++ src/sys_tool.py
@@ -0,0 +1,15 @@
+﻿import sys
+
+
+def run():
+	if len(sys.argv) > 1:
+		if sys.argv[1] in ["-h", "--help"]:
+			print("Для виведення тексту, виконайте скрипт без аргументів")
+
+		return
+
+	print("командна строка")
+
+
+if __name__ == "__main__":
+	run()
